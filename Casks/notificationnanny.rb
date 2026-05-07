18d9bd513af045b6e0037d34455699cbc332a390
e574358 <caspar.staehlin@sbb.ch>
2026-05-07 14:59:28 +0200

    cask: bump to v4.0.0

--- Casks/notificationnanny.rb
+++ Casks/notificationnanny.rb
@@ -1,6 +1,6 @@
 cask "notificationnanny" do
-  version "3.2.0"
-  sha256 "3ac501f3e159c0f70ff0628eaa416513c22c98664c7865ca30e627a5202308dd"
+  version "4.0.0"
+  sha256 "eaa4999bbe86d21926dee750fb4e7ffac4e31493e89166f1760e3dd777d7a59e"
 
   url "https://github.com/chessper53/NotificationNanny/releases/download/v#{version}/NotificationNanny-#{version}.zip"
   name "NotificationNanny"
